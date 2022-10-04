2720b65efb045e8887b446c45fe374983d02ba88
RHenrique <roosevelthenrique22@gmail.com>
2022-10-03 22:18:46 -0300

    feat: Models e Repositorys

--- jogo/src/main/java/br/edu/ifpb/project/quiz/jogo/repository/PerguntaRepository.java
+++ jogo/src/main/java/br/edu/ifpb/project/quiz/jogo/repository/PerguntaRepository.java
@@ -1,9 +1,11 @@
 package br.edu.ifpb.project.quiz.jogo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.edu.ifpb.project.quiz.jogo.model.Pergunta;
 
+@Repository
 public interface PerguntaRepository extends JpaRepository<Pergunta, Long> {
     
 }
--- jogo/src/main/java/br/edu/ifpb/project/quiz/jogo/repository/RespostaRepository.java
+++ jogo/src/main/java/br/edu/ifpb/project/quiz/jogo/repository/RespostaRepository.java
@@ -1,9 +1,11 @@
 package br.edu.ifpb.project.quiz.jogo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.edu.ifpb.project.quiz.jogo.model.Resposta;
 
+@Repository
 public interface RespostaRepository extends JpaRepository<Resposta, Long> {
     
 }
